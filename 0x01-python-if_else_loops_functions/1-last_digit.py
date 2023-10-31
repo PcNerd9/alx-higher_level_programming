b9d76d1d54b61d1cec0f487599b9f12c49cee965
PcNerd <ajayihabeeb977@gmail.com>
2023-10-31 20:22:54 +0100

    wrote a python3 script that print all the alphabet of a strig

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,15 +1,18 @@
 #!/usr/bin/python3
 import random
+
 number = random.randint(-10000, 10000)
+number = 0
 if number < 0:
     remainder = (-1 * number) % 10
+    remainder = remainder * -1
 else:
     remainder = number % 10
 string = f"Last digit of {number} is {remainder}"
 if remainder > 5:
     string = string + " " + "and is greater than 5"
 elif remainder == 0:
-    strings = string + " " + "and is 0"
+    string = string + " " + "and is 0"
 elif remainder < 6 and not 0:
-    string = string  + " " + "and is less than 6 and not 0"
+    string = string + " " + "and is less than 6 and not 0"
 print(string)
