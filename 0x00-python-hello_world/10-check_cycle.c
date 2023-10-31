760127d43e323290ef3abe800481136efaeb6c70
PcNerd <ajayihabeeb977@gmail.com>
2023-10-31 12:07:59 +0100

    resolve the bug in the 10-check_cycle.c file

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -1,18 +1,23 @@
 #include "lists.h"
 
-
+/**
+ * check_cycle - check if a cycle is present in linked
+ * list
+ * @list: the head to the linked list
+ * Return: 0 if not present otherwise return 1
+ */
 int check_cycle(listint_t *list)
 {
-        listint_t *hare, *tortise;
+	listint_t *hare, *tortise;
 
-        hare = list;
-        tortise = list;
-        while (hare)
-        {
-                hare = (hare->next)->next;
-                tortise = tortise->next;
-                if (hare == tortise)
-                  return (1);
-        }
-  return (0);
+	hare = list;
+	tortise = list;
+	while (hare)
+	{
+		hare = (hare->next)->next;
+		tortise = tortise->next;
+		if (hare == tortise)
+		return (1);
+	}
+	return (0);
 }
