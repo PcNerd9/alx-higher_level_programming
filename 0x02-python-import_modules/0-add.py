aed0e83abfb294ddb24807e5466cf0a03508aad1
PcNerd <ajayihabeeb977@gmail.com>
2023-11-02 17:26:58 +0100

    resolve the requirement issue in 0 and 1 file

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
-import add_0 as addition
 if __name__ == "__main__":
+    import add_0 as addition
     a = 1
     b = 2
     print("{0} + {1} = {2}".format(a, b, addition.add(a, b)))
