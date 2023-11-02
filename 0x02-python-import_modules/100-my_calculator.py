f59061fa7367ac379ce121afe5a2aa6b9a71214b
PcNerd <ajayihabeeb977@gmail.com>
2023-11-02 18:01:21 +0100

    resolve a requirement fail issue

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,29 +1,30 @@
 #!/usr/bin/python3
 
-from calculator_1 import add, sub, mul, div
-from sys import argv, exit
+if __name__ == "__main__":
+    from calculator_1 import add, sub, mul, div
+    from sys import argv, exit
 
-operator = ['+', '-', '*', '/']
-no_args = len(argv)
-if (no_args != 4):
-    print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-    exit(1)
-elif (argv[2] not in operator):
-    print(argv[2])
-    print("Unknown operator. Available operators: +, -, * and /")
-    exit(1)
+    operator = ['+', '-', '*', '/']
+    no_args = len(argv)
+    if (no_args != 4):
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        exit(1)
+    elif (argv[2] not in operator):
+        print(argv[2])
+        print("Unknown operator. Available operators: +, -, * and /")
+        exit(1)
 
-a = int(argv[1])
-b = int(argv[3])
+    a = int(argv[1])
+    b = int(argv[3])
 
-match argv[2]:
-    case '+':
-        result = add(a, b)
-    case '-':
-        result = sub(a, b)
-    case '*':
-        result = mul(a, b)
-    case '/':
-        result = div(a, b)
-print("{} + {} = {}".format(a, b, result))
-exit(0)
+    match argv[2]:
+        case '+':
+            result = add(a, b)
+        case '-':
+            result = sub(a, b)
+        case '*':
+            result = mul(a, b)
+        case '/':
+            result = div(a, b)
+    print("{} + {} = {}".format(a, b, result))
+    exit(0)
