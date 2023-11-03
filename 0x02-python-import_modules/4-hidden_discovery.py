41241ef1b5b20ccef28934f56f16e445a3ed7a93
PcNerd9 <132769837+PcNerd9@users.noreply.github.com>
2023-11-03 01:24:31 +0100

    Update 4-hidden_discovery.py

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -4,8 +4,7 @@ import hidden_4
 import dir
 
 if __name__ == "__main__":
-    list_of_variable = dir(hidden_4)
-    list_of_variable.sort()
+    list_of_variable = dir(hidden_4
     for variable in list_of_variable:
-        if variable[0] != "_":
+        if variable[0] != "_" and variabel[1] != "_":
             print(variable)
