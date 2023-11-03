578eb86374c460c11c939fbcd73a4c25bf3d6dca
PcNerd <ajayihabeeb977@gmail.com>
2023-11-03 12:31:09 +0100

    resolvethe bug in 100

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -16,16 +16,15 @@ if __name__ == "__main__":
 
     a = int(argv[1])
     b = int(argv[3])
-
+    op = argv[2]
     result = 0
-    match argv[2]:
-        case '+':
-            result = add(a, b)
-        case '-':
-            result = sub(a, b)
-        case '*':
-            result = mul(a, b)
-        case '/':
-            result = div(a, b)
+    if op == '+':
+        result = add(a, b)
+    elif op == '-':
+        result = sub(a, b)
+    elif op == '*':
+        result = mul(a, b)
+    elif op == '/':
+        result = div(a, b)
     print("{} {} {} = {}".format(a, argv[2], b, result))
     exit(0)
