6b9124c4ab0d0a45743816da8343530d5c50c07e
PcNerd <ajayihabeeb977@gmail.com>
2023-11-20 21:54:38 +0100

    wrote a pythone function that execute a funtion safely

---
+++ 0x05-python-exceptions/101-safe_function.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+import sys
+
+def safe_function(fct, *args):
+    try:
+        result = fct(args[0], args[1])
+    except Exception as e:
+        result = None
+        sys.stderr.write("Exception: {}\n".format(str(e)))
+    return result
