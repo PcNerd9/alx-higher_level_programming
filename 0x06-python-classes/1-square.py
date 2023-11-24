e9d2b57741e6fd1794c82585f2b036af1bf3ab05
PcNerd <ajayihabeeb977@gmail.com>
2023-11-24 01:58:46 +0100

    add documentation to 1-square.py file

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,5 +1,11 @@
 #!/usr/bin/python3
 
+""" Defines a square with a private
+instance attribute
+"""
+
+
 class Square:
+    """Defines a square with a size"""
     def __init__(self, size=0):
         self.__size = size
