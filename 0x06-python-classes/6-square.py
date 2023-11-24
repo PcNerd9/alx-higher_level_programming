333f498a14477077100f3203b1a789fc36aa8864
PcNerd <ajayihabeeb977@gmail.com>
2023-11-24 02:13:20 +0100

    add a documentation to the 6-square.py file

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -1,6 +1,17 @@
 #!/usr/bin/python3
 
+
+"""
+Defines a square with a private
+instance attribute
+"""
+
+
 class Square:
+    """ defines a square with a size property,
+    compute the current area of the square,
+    and print the square using # with it appropriate
+    co-ordinate"""
     def __init__(self, size=0, position=(0, 0)):
         self.__size = size
         self.__position = position
