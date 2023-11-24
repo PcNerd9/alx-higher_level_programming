753df2634f7b1683c420e7d6207c9992591ae4e5
PcNerd <ajayihabeeb977@gmail.com>
2023-11-24 02:00:49 +0100

    add documentation to the 2-square.py file

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,6 +1,13 @@
 #!/usr/bin/python3
 
+"""
+Defines a square with a private
+instance attribute
+"""
+
 class Square:
+    """define a square with only positive
+    integers"""
     def __init__(self, size=0):
         if (type(size) != int):
             raise TypeError("size must be an integer")
