e0305d9984d0f201785441ca6f119c3c5647babd
PcNerd <ajayihabeeb977@gmail.com>
2023-11-29 02:16:36 +0100

    add documentation

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,3 +1,15 @@
 #!/usr/bin/python3
+"""
+contain a single class
+LockedClass - prevent user from
+dynamically creating a new instance
+attribute
+"""
+
+
 class LockedClass:
+    """
+    Prevent user from dynamicall creating a new intance
+    attribute
+    """
     __slots__ = ("first_name")
