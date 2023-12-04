e0136b58f4dfee151e03391363fba5f929b606f8
PcNerd <ajayihabeeb977@gmail.com>
2023-12-04 21:46:06 +0100

    add to the rectangle class

---
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+
+"""
+contain only one class:
+    Rectangle: represent the real life rectangle
+    shape
+"""
+BaseGeometry = __import__("7-base_geometry").BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """
+    This class model the real life
+    rectangle shap.
+    it has a method that compute the area
+    and a magic method(__str__)for string
+    representation
+    """
+    def __init__(self, width, height):
+        if (type(width) != int):
+            raise TypeError("width must be an integer")
+        elif (width <= 0):
+            raise ValueError("width must be greater than 0")
+        if (type(height) != int):
+            raise TypeError("height must be an integer")
+        elif (height <= 0):
+            raise ValueError("height must be greater than 0")
+        self.__height = height
+        self.__width = width
+
+    def area(self):
+        return (self.__width * self.__height)
+
+    def __str__(self):
+        return "[Rectangle] {}/{}".format(self.__width, self.__height)
