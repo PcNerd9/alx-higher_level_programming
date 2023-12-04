c9ecf9ce0399766ea4c48262ceb003acf397f90e
PcNerd <ajayihabeeb977@gmail.com>
2023-12-04 20:06:27 +0100

    add a method the basegeometry class the method validate the integer that is passed

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+"""
+contain only one class:
+    BaseGeometry: valide the value integer
+    passed is true
+"""
+
+
+class BaseGeometry:
+    """
+    validate the value integer passed is true
+    or not
+    """
+    def area(self):
+        raise Exception("area() is not implemented")
+    def integer_validator(self, name, value):
+        if (type(value) != int):
+            raise TypeError("<name> must be an integer")
+        elif (value <= 0):
+            raise ValueError("<name> must be greater than 0")
