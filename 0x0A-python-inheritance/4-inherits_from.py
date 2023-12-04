dcb0bfabb29b4caece7179c89e0b454e7863b61d
PcNerd <ajayihabeeb977@gmail.com>
2023-12-04 18:48:15 +0100

    create an empty class

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -15,4 +15,4 @@ def inherits_from(obj, a_class):
     class that inherited from the specified class
     otherwise return False
     """
-    return issubclass(obj, a_class)
+    return issubclass(type(obj), a_class)
---
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+"""
+contains only one class:
+    BaseGeometry: empty
+"""
+
+
+class BaseGeometry:
+    """
+    empty
+    """
+    pass
