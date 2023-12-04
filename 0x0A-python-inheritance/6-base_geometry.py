8a01baa401b5ba36f1792f8c281439be25d8dcad
PcNerd <ajayihabeeb977@gmail.com>
2023-12-04 18:51:22 +0100

    add method to the created classn

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+"""
+contain only one class:
+    BaseGeometry:
+    empty
+"""
+
+
+class BaseGeometry:
+    """
+    empty and raise
+    an exception that area function is not implemented
+    """
+    def area(self):
+        raise Exception("area() is not implemented")
