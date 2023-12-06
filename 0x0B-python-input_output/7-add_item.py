0cae0e4910fa03de1385d386e5332d0fc09c5e4f
PcNerd <ajayihabeeb977@gmail.com>
2023-12-06 14:50:06 +0100

    resolve the bug in 7-add_item.py file

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -3,11 +3,10 @@
 """
 contain only one function
 """
-
-save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
-load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
 import sys
 import json
+save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
+load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
 
 
 def main():
@@ -15,9 +14,14 @@ def main():
     add all arguments to a python list and then
     save them to a file
     """
-    python_list = load_from_json_file("add_item.json")
-    save_to_json_file(python_list + sys.argv[1:], "add_item.json")
+    try:
+        python_list = load_from_json_file("add_item.json")
+    except FileNotFoundError:
+        python_list = []
+    finally:
+        save_to_json_file(python_list + sys.argv[1:], "add_item.json")
     print(load_from_json_file("add_item.json"))
+
 
 if __name__ == "__main__":
     main()
