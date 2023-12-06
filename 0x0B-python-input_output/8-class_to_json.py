d3418abca2f24bc00852fb7ecab196025e368c62
PcNerd <ajayihabeeb977@gmail.com>
2023-12-06 16:15:19 +0100

    create a python class

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -10,14 +10,15 @@ contain only one function:
 
 def class_to_json(obj):
     """
-    returns a dictionary description with simple data structure(list, dictionary
+    returns a dictionary description
+    with simple data structure(list, dictionary
     string integer and boolean) for Json serialization
     of an object
     """
     try:
         obj_dict = obj.__dict__
     except AttributeError:
-        return(obj)
+        return (obj)
     dict_ = {}
     for key, value in obj_dict.items():
         dict_[key] = value
--- 0x0B-python-input_output/main.py
+++ 0x0B-python-input_output/main.py
@@ -1,34 +1,12 @@
 #!/usr/bin/python3
-""" My class module
-"""
+Student = __import__('9-student').Student
 
-class_to_json = __import__('8-class_to_json').class_to_json
-class MyClass:
-    """ My class
-    """
+students = [Student("John", "Doe", 23), Student("Bob", "Dylan", 27)]
 
-    score = 0
-
-    def __init__(self, name, number = 4):
-        self.__name = name
-        self.number = number
-        self.is_team_red = (self.number % 2) == 0
-
-    def win(self):
-        self.score += 1
-
-    def lose(self):
-        self.score -= 1
-
-    def __str__(self):
-        return "[MyClass] {} - {:d} => {:d}".format(self.__name, self.number, self.score)
-
-
-m = MyClass("John")
-m.win()
-print(type(m))
-print(m)
-
-mj = class_to_json(m)
-print(type(mj))
-print(mj)
+for student in students:
+    j_student = student.to_json()
+    print(type(j_student))
+    print(j_student['first_name'])
+    print(type(j_student['first_name']))
+    print(j_student['age'])
+    print(type(j_student['age']))
