0af28d8a77edb4bbc08d99aa1d4116268cb6df18
PcNerd <ajayihabeeb977@gmail.com>
2024-01-17 05:22:31 +0100

    resolving the bug in 9-full_creation.sql

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,7 +1,4 @@
 -- creates a table second_table in the database
 -- and add multiple rows
 CREATE TABLE IF NOT EXISTS second_table (id int, name varchar(256), score int);
-INSERT INTO second_table (id, name, score) VALUE (1, "John", 10);
-INSERT INTO second_table (id, name, score) VALUE (2, "Alex", 3);
-INSERT INTO second_table (id, name, score) VALUE (3, "Bob", 14);
-INSERT INTO second_table (id, name, score) VALUE (4 "George", 8);
+INSERT INTO second_table (id, name, score) VALUE (1, "John", 10), (2, "Alex", 3), (3, "Bob", 14), (4, "George", 8);
