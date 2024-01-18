503186e2ceec7fcb208d0656e4976659053160d0
PcNerd <ajayihabeeb977@gmail.com>
2024-01-18 23:39:56 +0100

    wrote series of sql scripts for mysql

--- 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -1,6 +1,6 @@
 -- left join two tables to display it content
    SELECT ts.title, tsg.genre_id
-     FROM tv_shows as ts
-LEFT JOIN tv_show_genres as tsg
+     FROM tv_shows AS ts
+LEFT JOIN tv_show_genres AS tsg
        ON ts.id = tsg.show_id
  ORDER BY ts.title, tsg.genre_id;
