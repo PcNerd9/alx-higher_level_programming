c5ea37dd5f8619818d9de714d883ffab186a0f19
Habeeb Ajayi <Ajayihabeeb977@gmail.com>
2024-02-13 18:43:50 +0100

    wrote a script that prints the addition of 2 integers

---
+++ 0x12-javascript-warm_up/9-add.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+
+function add (a, b) {
+  return (a + b);
+}
+const num1 = Number(process.argv[2]);
+const num2 = Number(process.argv[3]);
+const result = add(num1, num2);
+console.log(result);
