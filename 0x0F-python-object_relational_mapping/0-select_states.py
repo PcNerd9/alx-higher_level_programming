8d482c3a87fa2e17a00937020dcde36152e49e71
Habeeb Ajayi <Ajayihabeeb977@gmail.com>
2024-02-16 17:08:41 +0100

    debugging

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -11,12 +11,16 @@ def main():
     """
     the main funcion of the program
     """
-    user = sys.argv[1]
-    password = sys.argv[2]
-    database = sys.argv[3]
+    try:
+        user = sys.argv[1]
+        password = sys.argv[2]
+        database = sys.argv[3]
 
-    db = MySQLdb.connect(host='localhost', port=3306,
-                         user=user, password=password, database=database)
+        db = MySQLdb.connect(host='localhost', port=3306,
+                             user=user, password=password, database=database)
+    except Exception as e:
+        print(e)
+
     cur = db.cursor()
     cur.execute("SELECT * FROM State ORDER BY id")
     rows = cur.fetchall()
