dcb783db3ac75d744397ededcc1bbed30e1b288d
Habeeb Ajayi <Ajayihabeeb977@gmail.com>
2024-02-16 16:33:21 +0100

    makeing my code to be pycodestyle standard

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -3,8 +3,10 @@
 """
 contain a function that query a database
 """
+
 import MySQLdb
 import sys
+
 
 def main():
     """
@@ -14,12 +16,14 @@ def main():
     password = sys.argv[2]
     database = sys.argv[3]
 
-    db = MySQLdb.connect(host='localhost', port=3306, user=user, password=password, database=database)
+    db = MySQLdb.connect(host='localhost', port=3306,
+                         user=user, password=password, database=database)
     cur = db.cursor()
     cur.execute("SELECT * FROM State ORDER BY id")
     rows = cur.fetchall()
     for row in rows:
         print(row)
+
 
 if __name__ == "__main__":
     main()
