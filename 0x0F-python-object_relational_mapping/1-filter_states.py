20d6732a90ea95c3417da85f311ca09cb3e36719
Habeeb Ajayi <Ajayihabeeb977@gmail.com>
2024-02-16 17:16:15 +0100

    debuggin 1-file

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -7,6 +7,7 @@ table
 import MySQLdb
 import sys
 
+
 def main():
     """
     the main function for the program
@@ -14,13 +15,15 @@ def main():
     user = sys.argv[1]
     password = sys.argv[2]
     database = sys.argv[3]
-    db = MySQLdb.connect(host="localhost", port=3306, user=user, password=password, database=database)
+    db = MySQLdb.connect(host="localhost", port=3306,
+                         user=user, password=password, database=database)
     cur = db.cursor()
     cur.execute("SELECT * FROM State ORDER BY id")
     rows = cur.fetchall()
     for row in rows:
         if (row[1].startswith("N")):
             print(row)
+
 
 if __name__ == "__main__":
     main()
