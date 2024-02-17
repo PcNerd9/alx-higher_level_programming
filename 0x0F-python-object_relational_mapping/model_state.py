d0f6df6065f75ca903c2feb8d34cb6ecae148800
Habeeb Ajayi <Ajayihabeeb977@gmail.com>
2024-02-17 21:56:55 +0100

    resolve the error in the model_state.py file

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -5,7 +5,7 @@ State class inheriting from sqlalchemy base
 from sqlalchemy import create_engine, Column, String, Interger
 from sqlalchemy.ext.declarative import declarative_base
 
-Base = declarateive_base()
+Base = declarative_base()
 
 
 class State(Base):
---
+++ 0x0F-python-object_relational_mapping/testing.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+
+import sqlalchemy
+print("it seems it ran successfully")
