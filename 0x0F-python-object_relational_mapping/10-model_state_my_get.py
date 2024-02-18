fc9b0eb95c64dc355a9761e16d9b4de4e1c2ea5e
Habeeb Ajayi <Ajayihabeeb977@gmail.com>
2024-02-18 17:39:11 +0100

    handle sql injection

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -14,12 +14,13 @@ def main():
     """
     The entry of the main program
     """
+    argument = (sys.argv[4],)
     engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}".format(
         sys.argv[1], sys.argv[2], sys.argv[3]),
                            pool_pre_ping=True)
     session = Session(engine)
     states = session.query(State).filter_by(
-            name=sys.argv[4]).order_by(State.id)
+            name=argument[0]).order_by(State.id)
     if (states.count() != 0):
         for state in states:
             print(state.id)
