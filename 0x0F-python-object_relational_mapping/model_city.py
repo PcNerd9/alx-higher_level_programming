1b7619e7a99c974cc4b39a1165212923a99c8439
Habeeb Ajayi <Ajayihabeeb977@gmail.com>
2024-02-18 19:41:41 +0100

    add documentation

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -4,7 +4,6 @@
 create a new model cities into the hbtn_0e_6_usa
 """
 
-
 from sqlalchemy import create_engine, Column, String, Integer, ForeignKey
 from sqlalchemy.orm import Session
 from model_state import Base, State
@@ -12,11 +11,12 @@ import sys
 
 
 class City(Base):
+    """
+    The class defination that will map to the
+    cities table in the databse
+    """
     __tablename__ = "cities"
 
     id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
     name = Column(String(128), nullable=False)
     state_id = Column(Integer, ForeignKey(State.id))
-
-
-
