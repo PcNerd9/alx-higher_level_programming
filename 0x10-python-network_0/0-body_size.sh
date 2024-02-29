89945ce2ef6a20aed1875634125ce0eb49abcc2d
Habeeb Ajayi <Ajayihabeeb977@gmail.com>
2024-02-29 22:44:54 +0100

    resolve requirement issues

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,22 +1,3 @@
 #!/usr/bin/env bash
 #takes in a url, sends a request to that url
-#and displays the size of the body of the response
-
-
-if [ $# -lt 1 ];
-then
-	echo "Usage: $0 <URL>"
-	exit 1
-fi;
-
-url=$1
-
-response_file=$(mktemp)
-if ! curl -s -o "$response_file" "$url";
-then
-	echo "Failed to get the url"
-fi;
-
-size=$(wc -c < "$response_file")
-
-echo "$size"
+curl -s $1 | wc -c
