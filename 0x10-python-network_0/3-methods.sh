f313d5da50039e54aa588b8050952bc0c1155fa0
Habeeb Ajayi <Ajayihabeeb977@gmail.com>
2024-02-29 23:40:40 +0100

    debugging v3

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #takes a specific url and displays all HTTP methods the server accept
-curl -sI -X OPTIONS "$1" | cut -d "," -f 2- | cut -d " " -f 2-
+curl -sI -X OPTIONS "$1" |grep "Allow" | cut -d "," -f 2- | cut -d " " -f 2-
