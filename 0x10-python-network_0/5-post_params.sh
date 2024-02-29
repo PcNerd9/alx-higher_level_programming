194f3fb51922b0058414658b4776ce0dc9f7fe5f
Habeeb Ajayi <Ajayihabeeb977@gmail.com>
2024-03-01 00:23:26 +0100

    debugging v3

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #sends a POST request to a specific URL and displays the body of the respons
-curl -X POST -d "email=test@gmail.com&subject=I+will+always+be+here+for+PLD" "$1"
+curl -X POST -d "email=test@gmail.com&subject=I will always be here for PLD" "$1"
