ddf76374c25fef46a5461f29bccf53516ebcd700
Habeeb Ajayi <Ajayihabeeb977@gmail.com>
2024-03-11 13:09:45 +0100

    remove the old version of 0x12-javascript-warm_up repo

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+
+def find_peak(list_of_integers):
+    if ( not isinstance(list_of_integers, list)):
+        return None
+
+    if (len(list_of_integers) == 0):
+        return None
+
+
--- 0x12-javascript-warm_up/9-add.js
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/node
-
-function add(a, b) {
-  return (a + b);
-}
-let num1 = Number(process.argv[2]);
-let num2 = Number(process.argv[3]);
-let result = add(num1, num2);
-console.log(result);
