36a2ce9d36f69c77f35f1a39f8005c60fc860db8
Habeeb Ajayi <Ajayihabeeb977@gmail.com>
2024-03-20 16:01:56 +0100

    catch exceptions in 5-filter_cities.py file

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -16,18 +16,21 @@ def main():
     password = sys.argv[2]
     database = sys.argv[3]
     argument = sys.argv[4]
-    db = MySQLdb.connect(host="localhost", port=3306, user=username,
-                         password=password, database=database)
-    cur = db.cursor()
-    cur.execute("SELECT cities.name FROM cities JOIN states WHERE\
+    try:
+        db = MySQLdb.connect(host="localhost", port=3306, user=username,
+                             password=password, database=database)
+        cur = db.cursor()
+        cur.execute("SELECT cities.name FROM cities JOIN states WHERE\
             states.id = cities.state_id AND states.name = %s\
             ORDER BY cities.id", (argument,))
-    states = cur.fetchall()
-    if (len(states) > 0):
-        for i in range(len(states) - 1):
-            print(states[i][0], end=", ")
+        states = cur.fetchall()
+        if (len(states) > 0):
+            for i in range(len(states) - 1):
+                print(states[i][0], end=", ")
 
-        print(states[len(states) - 1][0])
+            print(states[len(states) - 1][0])
+    except Exception as e:
+        print(e)
 
 
 if __name__ == "__main__":
