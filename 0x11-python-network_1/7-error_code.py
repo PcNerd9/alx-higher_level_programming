36fd966e62ca65523a4865fae35587b519b45c4e
Habeeb Ajayi <Ajayihabeeb977@gmail.com>
2024-03-29 15:14:29 +0100

    resolve pycodestyle requirement issues

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -7,7 +7,7 @@ sends a requests to a url and display response data
 def main():
     import sys
     import requests
-    
+
     response = requests.get(sys.argv[1])
     if (response.status_code >= 400):
         print("Error code: {}".format(response.status_code))
