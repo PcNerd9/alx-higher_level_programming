1fe1196c666408c5a5572d189fc291066a100193
Habeeb Ajayi <Ajayihabeeb977@gmail.com>
2024-03-29 14:41:36 +0100

    wrote a script that sends request to url and displays the value of the variable

---
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+sends a request to the url and display the value of the variable
+"""
+
+
+def main():
+    import sys
+    import requests
+
+    response = requests.get(sys.argv[1])
+    print(response.headers["X-Request-Id"])
+
+
+if __name__ == "__main__":
+    main()
