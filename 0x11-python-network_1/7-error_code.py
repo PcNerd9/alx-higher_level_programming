3b8cd741ac35453f84202198f7a07c8eeee5a852
Habeeb Ajayi <Ajayihabeeb977@gmail.com>
2024-03-29 15:10:06 +0100

    wrote a python scritp that send a request to a url

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+sends a requests to a url and display response data
+"""
+
+
+def main():
+    import sys
+    import requests
+    
+    response = requests.get(sys.argv[1])
+    if (response.status_code >= 400):
+        print("Error code: {}".format(response.status_code))
+    else:
+        print(response.text)
+
+
+if __name__ == "__main__":
+    main()
