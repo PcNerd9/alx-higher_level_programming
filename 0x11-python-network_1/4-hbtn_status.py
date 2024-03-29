518a8d3b125d92b4e176e6adaa5ee09b8e41948c
Habeeb Ajayi <Ajayihabeeb977@gmail.com>
2024-03-29 14:25:38 +0100

    wrote a script that fetches url using requests library

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+fetches a url using request library
+"""
+
+
+def main():
+    import requests
+    import sys
+
+    response = requests.get(sys.argv[1])
+    print("Body response:")
+    print("\t- type: {}".format(type(response.text)))
+    print("\t- content: {}".format(response.text))
+
+
+if __name__ == "__main__":
+    main()
